f549f783041de6ef1a8637b8fc2cd1e503b31082
Slava Shklyaev <shk.slava@gmail.com>
2014-11-01 19:22:01 +0200

    Redesign

--- src/Data/OBDD/Internal.hs
+++ src/Data/OBDD/Internal.hs
@@ -14,8 +14,6 @@ newtype Id = Id Int
 newtype Var = Var Int
   deriving (Eq, Ord, Show)
 
-type RefPool = Map Id ROBDD
-
 type IdPool = Map (Var, Id, Id) Id
 
 newtype IdPoolM a = IdPoolM (State IdPool a)
@@ -24,54 +22,83 @@ newtype IdPoolM a = IdPoolM (State IdPool a)
 runIdPoolM :: IdPoolM a -> a
 runIdPoolM (IdPoolM s) = evalState s Map.empty
 
+type RefPool = Map Id ROBDD
+
+type RefPoolM a = StateT RefPool IdPoolM a
+
 type Binding = Map Var Bool
 
+data BranchType = Orig | Ref
+  deriving (Eq, Ord, Show)
+
 data ROBDD
-  = Branch Id Var ROBDD ROBDD
-  | Ref Id
+  = Branch BranchType Id Var ROBDD ROBDD
   | Leaf Bool
   deriving Eq
 
 getId :: ROBDD -> Id
-getId (Branch i _ _ _) = i
-getId (Ref i)          = i
-getId (Leaf False)     = Id 0
-getId (Leaf True)      = Id 1
+getId (Branch _ i _ _ _) = i
+getId (Leaf False)       = Id 0
+getId (Leaf True)        = Id 1
 
 instance Show Id where
   show (Id i) = "#" ++ show i
 
 instance Show ROBDD where
-  show (Branch i (Var v) lo hi) = "Branch" ++ show i ++ " " ++ show v
-                                   ++ " (" ++ show lo ++ ")"
-                                   ++ " (" ++ show hi ++ ")"
-  show (Ref i)                  = "Ref" ++ show i
-  show (Leaf True)              = "T"
-  show (Leaf False)             = "F"
-
-apply :: (Bool -> Bool -> Bool) -> ROBDD -> ROBDD -> IdPoolM ROBDD
-apply f = go
-  where
-    go = undefined
-    -- go x@(Branch xi xlo xhi) y@(Branch yi ylo yhi)
-    --   | xi < yi              = branch xi (go xlo y)   (go xhi y)
-    --   | yi < xi              = branch yi (go x   ylo) (go x   yhi)
-    --   | otherwise            = branch xi (go xlo ylo) (go xhi yhi)
-    -- go x (Branch yi ylo yhi) = branch yi (go x   ylo) (go x   yhi)
-    -- go (Branch xi xlo xhi) y = branch xi (go xlo y)   (go xhi y)
-    -- go (Leaf x) (Leaf y)   = Leaf $ f x y
-
-    -- branch i lo hi
-    --   | lo == hi  = lo
-    --   | otherwise = Branch i lo hi
-
-branch = undefined
+  show (Branch t i (Var v) lo hi) = showType t ++ show i ++ " " ++ show v
+                                     ++ " (" ++ show lo ++ ")"
+                                     ++ " (" ++ show hi ++ ")"
+    where
+      showType Orig = "Branch"
+      showType Ref  = "Ref"
+  show (Leaf True)                = "T"
+  show (Leaf False)               = "F"
 
 const :: Bool -> ROBDD
 const = Leaf
 
 varM :: Int -> IdPoolM ROBDD
-varM x = branch (Var x) (Leaf False) (Leaf True)
+varM x = evalStateT (branch (Var x) (Leaf False) (Leaf True)) Map.empty
+
+apply :: (Bool -> Bool -> Bool) -> ROBDD -> ROBDD -> IdPoolM ROBDD
+apply f a b = evalStateT (go a b) Map.empty
+  where
+    go :: ROBDD -> ROBDD -> RefPoolM ROBDD
+    go x@(Branch _ _ xv xlo xhi) y@(Branch _ _ yv ylo yhi)
+      | xv < yv                  = branch' xv (go xlo y)   (go xhi y)
+      | yv < xv                  = branch' yv (go x   ylo) (go x   yhi)
+      | otherwise                = branch' xv (go xlo ylo) (go xhi yhi)
+    go x (Branch _ _ yv ylo yhi) = branch' yv (go x   ylo) (go x   yhi)
+    go (Branch _ _ xv xlo xhi) y = branch' xv (go xlo y)   (go xhi y)
+    go (Leaf x) (Leaf y)         = return . Leaf $ f x y
+
+    branch' v loM hiM = do
+      lo <- loM
+      hi <- hiM
+      branch v lo hi
+
+nextId :: IdPoolM Id
+nextId = do
+  pool <- get
+  return . Id $ 2 + Map.size pool
+
+branch :: Var -> ROBDD -> ROBDD -> RefPoolM ROBDD
+branch v lo hi
+  | equals lo hi = return lo
+  | otherwise    = do
+      idPool <- lift get
+      case Map.lookup key idPool of
+        Just i  -> get >>= maybe (newBranch i) return . Map.lookup i
+        Nothing -> lift nextId >>= newBranch
+  where
+    key = (v, getId lo, getId hi)
+
+    newBranch i = do
+      lift . modify $ Map.insert key i
+      let origBranch = Branch Orig i v lo hi
+          refBranch  = Branch Ref  i v lo hi
+      modify $ Map.insert i refBranch
+      return origBranch
 
 notM :: ROBDD -> IdPoolM ROBDD
 notM = xorM (const True)
@@ -148,11 +175,11 @@ binary f x y = do
 restrict :: Var -> Bool -> ROBDD -> IdPoolM ROBDD
 restrict i v = undefined
   where
-    go x@(Branch _ xi lo hi)
-      | xi < i    = branch xi (go lo) (go hi)
-      | xi > i    = x
-      | otherwise = if v then hi else lo
-    go x = x
+    -- go x@(Branch _ xi lo hi)
+    --   | xi < i    = branch xi (go lo) (go hi)
+    --   | xi > i    = x
+    --   | otherwise = if v then hi else lo
+    -- go x = x
 
 -- exists :: Var -> ROBDD -> IdPoolM Bool
 -- exists i x = isTautology $ restrict i True x `or` restrict i False x
@@ -170,12 +197,11 @@ isContradiction :: ROBDD -> Bool
 isContradiction = (`equals` (const False))
 
 fold :: (Var -> a -> a -> a) -> a -> a -> ROBDD -> a
-fold f z0 z1 = go
+fold f z0 z1 = undefined
   where
     go (Leaf False)     = z0
     go (Leaf True)      = z1
-    go (Branch _ i lo hi) = f i (go lo) (go hi)
-    go _ = undefined
+    go (Branch _ _ i lo hi) = f i (go lo) (go hi)
 
 evaluate :: Binding -> ROBDD -> Bool
 evaluate env = fold f False True
