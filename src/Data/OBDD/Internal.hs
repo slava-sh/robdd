854f5958039ccf26e86106b098cdcceacd860b4c
Slava Shklyaev <shk.slava@gmail.com>
2014-11-01 20:49:46 +0200

    Strengthen type safety using a phantom type variable

--- src/Data/OBDD/Internal.hs
+++ src/Data/OBDD/Internal.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
 module Data.OBDD.Internal where
 
 import Prelude hiding (not, and, or, const)
@@ -9,43 +10,43 @@ import qualified Data.Map as Map
 import Control.Applicative
 import Control.Monad.State
 
-newtype Id = Id Int
+newtype Id p = Id Int
   deriving (Eq, Ord)
 
 newtype Var = Var Int
   deriving (Eq, Ord, Show)
 
-type IdPool = Map (Var, Id, Id) Id
+type IdPool p = Map (Var, Id p, Id p) (Id p)
 
-newtype IdPoolM a = IdPoolM (State IdPool a)
-  deriving (Functor, Applicative, Monad, MonadState IdPool)
+newtype IdPoolM p a = IdPoolM (State (IdPool p) a)
+  deriving (Functor, Applicative, Monad, MonadState (IdPool p))
 
-runIdPoolM :: IdPoolM a -> a
+runIdPoolM :: forall a. (forall p. IdPoolM p a) -> a
 runIdPoolM (IdPoolM s) = evalState s Map.empty
 
-type RefPool = Map Id ROBDD
+type RefPool p = Map (Id p) (ROBDD p)
 
-type RefPoolM a = StateT RefPool IdPoolM a
+type RefPoolM p a = StateT (RefPool p) (IdPoolM p) a
 
 type Binding = Map Var Bool
 
 data BranchType = Orig | Ref
   deriving (Eq, Ord, Show)
 
-data ROBDD
-  = Branch BranchType Id Var ROBDD ROBDD
+data ROBDD p
+  = Branch BranchType (Id p) Var (ROBDD p) (ROBDD p)
   | Leaf Bool
   deriving Eq
 
-getId :: ROBDD -> Id
+getId :: ROBDD p -> Id p
 getId (Branch _ i _ _ _) = i
 getId (Leaf False)       = Id 0
 getId (Leaf True)        = Id 1
 
-instance Show Id where
+instance Show (Id p) where
   show (Id i) = "#" ++ show i
 
-instance Show ROBDD where
+instance Show (ROBDD p) where
   show (Branch t i (Var v) lo hi) = showType t ++ show i ++ " " ++ show v
                                      ++ " (" ++ show lo ++ ")"
                                      ++ " (" ++ show hi ++ ")"
@@ -55,16 +56,16 @@ instance Show ROBDD where
   show (Leaf True)                = "T"
   show (Leaf False)               = "F"
 
-const :: Bool -> ROBDD
+const :: Bool -> ROBDD p
 const = Leaf
 
-varM :: Int -> IdPoolM ROBDD
+varM :: Int -> IdPoolM p (ROBDD p)
 varM x = evalStateT (branch (Var x) (Leaf False) (Leaf True)) Map.empty
 
-apply :: (Bool -> Bool -> Bool) -> ROBDD -> ROBDD -> IdPoolM ROBDD
+apply :: (Bool -> Bool -> Bool) -> ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 apply f a b = evalStateT (go a b) Map.empty
   where
-    go :: ROBDD -> ROBDD -> RefPoolM ROBDD
+    go :: ROBDD p -> ROBDD p -> RefPoolM p (ROBDD p)
     go x@(Branch _ _ xv xlo xhi) y@(Branch _ _ yv ylo yhi)
       | xv < yv                  = branchM xv (go xlo y)   (go xhi y)
       | yv < xv                  = branchM yv (go x   ylo) (go x   yhi)
@@ -73,13 +74,13 @@ apply f a b = evalStateT (go a b) Map.empty
     go (Branch _ _ xv xlo xhi) y = branchM xv (go xlo y)   (go xhi y)
     go (Leaf x) (Leaf y)         = return . Leaf $ f x y
 
-branchM :: Var -> RefPoolM ROBDD -> RefPoolM ROBDD -> RefPoolM ROBDD
+branchM :: Var -> RefPoolM p (ROBDD p) -> RefPoolM p (ROBDD p) -> RefPoolM p (ROBDD p)
 branchM v loM hiM = do
   lo <- loM
   hi <- hiM
   branch v lo hi
 
-restrict :: Var -> Bool -> ROBDD -> IdPoolM ROBDD
+restrict :: Var -> Bool -> ROBDD p -> IdPoolM p (ROBDD p)
 restrict variable value robdd = evalStateT (go robdd) Map.empty
   where
     go (Branch _ _ v lo hi)
@@ -89,19 +90,19 @@ restrict variable value robdd = evalStateT (go robdd) Map.empty
       | otherwise    = rebuild lo
     go x = return x
 
-rebuild :: ROBDD -> RefPoolM ROBDD
+rebuild :: ROBDD p -> RefPoolM p (ROBDD p)
 rebuild (Branch _ _ v lo hi) = branch v lo hi
 rebuild x                    = return x
 
-nextId :: IdPoolM Id
+nextId :: IdPoolM p (Id p)
 nextId = do
   pool <- get
   return . Id $ 2 + Map.size pool
 
-branch :: Var -> ROBDD -> ROBDD -> RefPoolM ROBDD
+branch :: Var -> ROBDD p -> ROBDD p -> RefPoolM p (ROBDD p)
 branch v lo hi
-  | equals lo hi = return lo
-  | otherwise    = do
+  | lo `equals` hi = return lo
+  | otherwise      = do
       idPool <- lift get
       case Map.lookup key idPool of
         Just i  -> get >>= maybe (newBranch i) return . Map.lookup i
@@ -116,22 +117,22 @@ branch v lo hi
       modify $ Map.insert i refBranch
       return origBranch
 
-notM :: ROBDD -> IdPoolM ROBDD
+notM :: ROBDD p -> IdPoolM p (ROBDD p)
 notM = xorM (const True)
 
-andM :: ROBDD -> ROBDD -> IdPoolM ROBDD
+andM :: ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 andM = apply (&&)
 
-orM :: ROBDD -> ROBDD -> IdPoolM ROBDD
+orM :: ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 orM = apply (||)
 
-xorM :: ROBDD -> ROBDD -> IdPoolM ROBDD
+xorM :: ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 xorM = apply (/=)
 
-iffM :: ROBDD -> ROBDD -> IdPoolM ROBDD
+iffM :: ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 iffM = apply (==)
 
-implM :: ROBDD -> ROBDD -> IdPoolM ROBDD
+implM :: ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)
 implM = apply (\x y -> Prelude.not x || y)
 
 data Expr
@@ -172,7 +173,7 @@ iff = EIff
 impl :: Expr -> Expr -> Expr
 impl = EImpl
 
-reduce :: Expr -> IdPoolM ROBDD
+reduce :: Expr -> IdPoolM p (ROBDD p)
 reduce (EConst x)   = return $ const x
 reduce (EVar   x)   = varM x
 reduce (ENot   x)   = reduce x >>= notM
@@ -182,37 +183,37 @@ reduce (EXor   x y) = binary xorM  x y
 reduce (EIff   x y) = binary iffM  x y
 reduce (EImpl  x y) = binary implM x y
 
-binary :: (ROBDD -> ROBDD -> IdPoolM ROBDD) -> Expr -> Expr -> IdPoolM ROBDD
+binary :: (ROBDD p -> ROBDD p -> IdPoolM p (ROBDD p)) -> Expr -> Expr -> IdPoolM p (ROBDD p)
 binary f x y = do
   x' <- reduce x
   y' <- reduce y
   f x' y'
 
-exists :: Var -> ROBDD -> IdPoolM Bool
+exists :: Var -> ROBDD p -> IdPoolM p Bool
 exists v x = isTautology <$> do
   xT <- restrict v True  x
   xF <- restrict v False x
   orM xT xF
 
-forall :: Var -> ROBDD -> IdPoolM Bool
+forall :: Var -> ROBDD p -> IdPoolM p Bool
 forall v x = isTautology <$> do
   xT <- restrict v True  x
   xF <- restrict v False x
   andM xT xF
 
-equals :: ROBDD -> ROBDD -> Bool
+equals :: ROBDD p -> ROBDD p -> Bool
 equals x y = getId x == getId y
 
-isTautology :: ROBDD -> Bool
+isTautology :: ROBDD p -> Bool
 isTautology = (`equals` (const True))
 
-isContradiction :: ROBDD -> Bool
+isContradiction :: ROBDD p -> Bool
 isContradiction = (`equals` (const False))
 
-fold :: forall a. (Var -> a -> a -> a) -> a -> a -> ROBDD -> a
+fold :: forall a p. (Var -> a -> a -> a) -> a -> a -> ROBDD p -> a
 fold f z0 z1 robdd = evalState (go robdd) Map.empty
   where
-    go :: ROBDD -> State (Map Id a) a
+    go :: ROBDD p -> State (Map (Id p) a) a
     go (Leaf False)            = return z0
     go (Leaf True)             = return z1
     go (Branch Ref i _ _ _)    = get >>= return . (! i)
@@ -221,7 +222,7 @@ fold f z0 z1 robdd = evalState (go robdd) Map.empty
       modify $ Map.insert i result
       return result
 
-evaluate :: Binding -> ROBDD -> Bool
+evaluate :: Binding -> ROBDD p -> Bool
 evaluate env = fold f False True
   where
     f i lo hi = case Map.lookup i env of
@@ -229,13 +230,13 @@ evaluate env = fold f False True
       Just True  -> hi
       Nothing    -> error "evaluate: incorrect binding"
 
-anySat :: ROBDD -> Maybe Binding
+anySat :: ROBDD p -> Maybe Binding
 anySat = fold f Nothing (Just Map.empty)
   where
     f i lo hi = Map.insert i False <$> lo
             <|> Map.insert i True  <$> hi
 
-allSat :: ROBDD -> [Binding]
+allSat :: ROBDD p -> [Binding]
 allSat = fold f [] [Map.empty]
   where
     f i lo hi = map (Map.insert i False) lo
