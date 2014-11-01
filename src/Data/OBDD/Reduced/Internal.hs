e8dceed1efbf2fcf6a59c561889ce4632eeaebd5
Slava Shklyaev <shk.slava@gmail.com>
2014-11-01 22:22:15 +0200

    Spell Robdd with all but the first letter in lowercase

--- src/Data/OBDD/Reduced/Internal.hs
+++ src/Data/OBDD/Reduced/Internal.hs
@@ -19,29 +19,29 @@ newtype Id p = Id Int
 
 type IdPool p = Map (Var, Id p, Id p) (Id p)
 
-newtype ROBDDM p a = ROBDDM (State (IdPool p) a)
+newtype RobddM p a = RobddM (State (IdPool p) a)
   deriving (Functor, Applicative, Monad, MonadState (IdPool p))
 
-runROBDDM :: forall a. (forall p. ROBDDM p a) -> a
-runROBDDM (ROBDDM s) = evalState s Map.empty
+runRobddM :: forall a. (forall p. RobddM p a) -> a
+runRobddM (RobddM s) = evalState s Map.empty
 
-type RefPool p = Map (Id p) (ROBDD p)
-type RefPoolM p a = StateT (RefPool p) (ROBDDM p) a
+type RefPool p = Map (Id p) (Robdd p)
+type RefPoolM p a = StateT (RefPool p) (RobddM p) a
 
 data BranchType = Orig | Ref
   deriving (Eq, Ord, Show)
 
-data ROBDD p
-  = Branch BranchType (Id p) Var (ROBDD p) (ROBDD p)
+data Robdd p
+  = Branch BranchType (Id p) Var (Robdd p) (Robdd p)
   | Leaf Bool
   deriving Eq
 
-getId :: ROBDD p -> Id p
+getId :: Robdd p -> Id p
 getId (Branch _ i _ _ _) = i
 getId (Leaf False)       = Id 0
 getId (Leaf True)        = Id 1
 
-instance Show (ROBDD p) where
+instance Show (Robdd p) where
   show (Leaf False)                    = "F"
   show (Leaf True)                     = "T"
   show (Branch t (Id i) (Var v) lo hi) = showType t ++ "#" ++ show i ++ " "
@@ -52,13 +52,13 @@ instance Show (ROBDD p) where
       showType Orig = "Branch"
       showType Ref  = "Ref"
 
-varM :: Int -> ROBDDM p (ROBDD p)
+varM :: Int -> RobddM p (Robdd p)
 varM x = evalStateT (branch (Var x) (Leaf False) (Leaf True)) Map.empty
 
-apply :: (Bool -> Bool -> Bool) -> ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+apply :: (Bool -> Bool -> Bool) -> Robdd p -> Robdd p -> RobddM p (Robdd p)
 apply f a b = evalStateT (go a b) Map.empty
   where
-    go :: ROBDD p -> ROBDD p -> RefPoolM p (ROBDD p)
+    go :: Robdd p -> Robdd p -> RefPoolM p (Robdd p)
     go x@(Branch _ _ xv xlo xhi) y@(Branch _ _ yv ylo yhi)
       | xv < yv                  = branchM xv (go xlo y)   (go xhi y)
       | yv < xv                  = branchM yv (go x   ylo) (go x   yhi)
@@ -67,13 +67,13 @@ apply f a b = evalStateT (go a b) Map.empty
     go (Branch _ _ xv xlo xhi) y = branchM xv (go xlo y)   (go xhi y)
     go (Leaf x) (Leaf y)         = return . Leaf $ f x y
 
-branchM :: Var -> RefPoolM p (ROBDD p) -> RefPoolM p (ROBDD p) -> RefPoolM p (ROBDD p)
+branchM :: Var -> RefPoolM p (Robdd p) -> RefPoolM p (Robdd p) -> RefPoolM p (Robdd p)
 branchM v loM hiM = do
   lo <- loM
   hi <- hiM
   branch v lo hi
 
-restrict :: Var -> Bool -> ROBDD p -> ROBDDM p (ROBDD p)
+restrict :: Var -> Bool -> Robdd p -> RobddM p (Robdd p)
 restrict variable value robdd = evalStateT (go robdd) Map.empty
   where
     go (Branch _ _ v lo hi)
@@ -83,16 +83,16 @@ restrict variable value robdd = evalStateT (go robdd) Map.empty
       | otherwise    = rebuild lo
     go x = return x
 
-rebuild :: ROBDD p -> RefPoolM p (ROBDD p)
+rebuild :: Robdd p -> RefPoolM p (Robdd p)
 rebuild (Branch _ _ v lo hi) = branch v lo hi
 rebuild x                    = return x
 
-nextId :: ROBDDM p (Id p)
+nextId :: RobddM p (Id p)
 nextId = do
   pool <- get
   return . Id $ 2 + Map.size pool
 
-branch :: Var -> ROBDD p -> ROBDD p -> RefPoolM p (ROBDD p)
+branch :: Var -> Robdd p -> Robdd p -> RefPoolM p (Robdd p)
 branch v lo hi
   | lo `equals` hi = return lo
   | otherwise      = do
@@ -110,25 +110,25 @@ branch v lo hi
       modify $ Map.insert i refBranch
       return origBranch
 
-const :: Bool -> ROBDD p
+const :: Bool -> Robdd p
 const = Leaf
 
-notM :: ROBDD p -> ROBDDM p (ROBDD p)
+notM :: Robdd p -> RobddM p (Robdd p)
 notM = (`xorM` (const True))
 
-andM :: ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+andM :: Robdd p -> Robdd p -> RobddM p (Robdd p)
 andM = apply (&&)
 
-orM :: ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+orM :: Robdd p -> Robdd p -> RobddM p (Robdd p)
 orM = apply (||)
 
-xorM :: ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+xorM :: Robdd p -> Robdd p -> RobddM p (Robdd p)
 xorM = apply (/=)
 
-iffM :: ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+iffM :: Robdd p -> Robdd p -> RobddM p (Robdd p)
 iffM = apply (==)
 
-implM :: ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)
+implM :: Robdd p -> Robdd p -> RobddM p (Robdd p)
 implM = apply (\x y -> Prelude.not x || y)
 
 data Expr
@@ -169,7 +169,7 @@ iff = EIff
 impl :: Expr -> Expr -> Expr
 impl = EImpl
 
-reduce :: Expr -> ROBDDM p (ROBDD p)
+reduce :: Expr -> RobddM p (Robdd p)
 reduce (EConst x)   = return $ const x
 reduce (EVar   x)   = varM x
 reduce (ENot   x)   = reduce x >>= notM
@@ -179,37 +179,37 @@ reduce (EXor   x y) = binary xorM  x y
 reduce (EIff   x y) = binary iffM  x y
 reduce (EImpl  x y) = binary implM x y
 
-binary :: (ROBDD p -> ROBDD p -> ROBDDM p (ROBDD p)) -> Expr -> Expr -> ROBDDM p (ROBDD p)
+binary :: (Robdd p -> Robdd p -> RobddM p (Robdd p)) -> Expr -> Expr -> RobddM p (Robdd p)
 binary f x y = do
   x' <- reduce x
   y' <- reduce y
   f x' y'
 
-exists :: Var -> ROBDD p -> ROBDDM p Bool
+exists :: Var -> Robdd p -> RobddM p Bool
 exists v x = isTautology <$> do
   xT <- restrict v True  x
   xF <- restrict v False x
   orM xT xF
 
-forall :: Var -> ROBDD p -> ROBDDM p Bool
+forall :: Var -> Robdd p -> RobddM p Bool
 forall v x = isTautology <$> do
   xT <- restrict v True  x
   xF <- restrict v False x
   andM xT xF
 
-equals :: ROBDD p -> ROBDD p -> Bool
+equals :: Robdd p -> Robdd p -> Bool
 equals x y = getId x == getId y
 
-isTautology :: ROBDD p -> Bool
+isTautology :: Robdd p -> Bool
 isTautology = (`equals` (const True))
 
-isContradiction :: ROBDD p -> Bool
+isContradiction :: Robdd p -> Bool
 isContradiction = (`equals` (const False))
 
-fold :: forall a p. (Var -> a -> a -> a) -> a -> a -> ROBDD p -> a
+fold :: forall a p. (Var -> a -> a -> a) -> a -> a -> Robdd p -> a
 fold f z0 z1 robdd = evalState (go robdd) Map.empty
   where
-    go :: ROBDD p -> State (Map (Id p) a) a
+    go :: Robdd p -> State (Map (Id p) a) a
     go (Leaf False)            = return z0
     go (Leaf True)             = return z1
     go (Branch Ref i _ _ _)    = get >>= return . (! i)
@@ -220,7 +220,7 @@ fold f z0 z1 robdd = evalState (go robdd) Map.empty
 
 type Binding = Map Var Bool
 
-evaluate :: Binding -> ROBDD p -> Bool
+evaluate :: Binding -> Robdd p -> Bool
 evaluate env = fold f False True
   where
     f i lo hi = case Map.lookup i env of
@@ -228,13 +228,13 @@ evaluate env = fold f False True
       Just True  -> hi
       Nothing    -> error "evaluate: incorrect binding"
 
-anySat :: ROBDD p -> Maybe Binding
+anySat :: Robdd p -> Maybe Binding
 anySat = fold f Nothing (Just Map.empty)
   where
     f i lo hi = Map.insert i False <$> lo
             <|> Map.insert i True  <$> hi
 
-allSat :: ROBDD p -> [Binding]
+allSat :: Robdd p -> [Binding]
 allSat = fold f [] [Map.empty]
   where
     f i lo hi = map (Map.insert i False) lo
--- tests/test.hs
+++ tests/test.hs
@@ -13,14 +13,14 @@ tests = testGroup "Data.OBDD.Reduced"
   --[ testCase "1" $ (impl (var 1) (var 2))
   --    @?= I.Branch (I.Var 1) (I.Leaf True)
   --          (I.Branch (I.Var 2) (I.Leaf False) (I.Leaf True))
-  -- Adapted from http://www.cs.otago.ac.nz/staffpriv/ok/COSC410/robdd.hs
-  [ testROBDD "O'Keefe 2" (or (var 1) (var 1)) (var 1)
-  , testROBDD "O'Keefe 3" (iff (impl (var 1) (var 2)) (or (not (var 1)) (var 2))) true
-  , testROBDD "O'Keefe 4" (iff (and (var 1) (var 1)) (var 1)) true
-  , testROBDD "O'Keefe 7"
+  -- Adapted from http://www.cs.otago.ac.nz/staffpriv/ok/COSC410/Robdd.hs
+  [ testRobdd "O'Keefe 2" (or (var 1) (var 1)) (var 1)
+  , testRobdd "O'Keefe 3" (iff (impl (var 1) (var 2)) (or (not (var 1)) (var 2))) true
+  , testRobdd "O'Keefe 4" (iff (and (var 1) (var 1)) (var 1)) true
+  , testRobdd "O'Keefe 7"
       (iff (not (or (var 1) (or (var 2) (var 3))))
            (and (not (var 1)) (and (not (var 2)) (not (var 3))))) true
-  , testROBDD "O'Keefe 8"
+  , testRobdd "O'Keefe 8"
       (iff (not (and (var 1) (and (var 2) (and (var 3) (and (var 4)
             (and (var 5) (and (var 6) (and (var 7) (and (var 8) (and (var 9)
             (and (var 10) (and (var 11) (and (var 12) (and (var 13) (and
@@ -30,7 +30,7 @@ tests = testGroup "Data.OBDD.Reduced"
             8)) (or (not (var 9)) (or (not (var 10)) (or (not (var 11)) (or
             (not (var 12)) (or (not (var 13)) (or (not (var 14)) (or (not
             (var 15)) (not (var 16)))))))))))))))))) true
-  , testROBDD "O'Keefe 9" (iff (not (or (var 1) (or (var 2) (or (var 3) (or (var 4) (or
+  , testRobdd "O'Keefe 9" (iff (not (or (var 1) (or (var 2) (or (var 3) (or (var 4) (or
             (var 5) (or (var 6) (or (var 7) (or (var 8) (or (var 9) (or (var
             10) (or (var 11) (or (var 12) (or (var 13) (or (var 14) (or (var
             15) (var 16))))))))))))))))) (and (not (var 1)) (and (not (var
@@ -39,36 +39,36 @@ tests = testGroup "Data.OBDD.Reduced"
             (not (var 9)) (and (not (var 10)) (and (not (var 11)) (and (not
             (var 12)) (and (not (var 13)) (and (not (var 14)) (and (not (var
             15)) (not (var 16)))))))))))))))))) true
-  , testROBDD "Pengelly 10" (and (var 1) (not (var 1))) false
-  , testROBDD "Pengelly 11"
+  , testRobdd "Pengelly 10" (and (var 1) (not (var 1))) false
+  , testRobdd "Pengelly 11"
       (or (and (var 1) (not (var 2))) (and (not (var 1)) (var 2)))
       (xor (var 1) (var 2))
-  , testROBDD "Pengelly 12" (and (and (var 1) (impl (var 1) (var 2))) (not (var 2))) false
-  , testROBDD "Pengelly 13" (and (and (not (var 2)) (impl (var 1) (var 2))) (var 1)) false
-  , testROBDD "Pengelly 14" (iff (var 1) (not (not (var 1)))) true
-  , testROBDD "Pengelly 15"
+  , testRobdd "Pengelly 12" (and (and (var 1) (impl (var 1) (var 2))) (not (var 2))) false
+  , testRobdd "Pengelly 13" (and (and (not (var 2)) (impl (var 1) (var 2))) (var 1)) false
+  , testRobdd "Pengelly 14" (iff (var 1) (not (not (var 1)))) true
+  , testRobdd "Pengelly 15"
       (iff (and (and (var 1) (var 2)) (var 3))
            (and (var 1) (and (var 2) (var 3)))) true
-  , testROBDD "Pengelly 16"
+  , testRobdd "Pengelly 16"
       (iff (not (or (var 1) (var 2))) (and (not (var 1)) (not (var 2)))) true
-  , testROBDD "Pengelly 17"
+  , testRobdd "Pengelly 17"
       (impl (impl (var 1) (var 3))
             (impl (impl (var 2) (var 4)) (impl (or (var 1) (var 2)) (var 3))))
       (or (or (or (var 1) (not (var 2))) (var 3)) (not (var 4)))
-  , testROBDD "Pengelly 18"
+  , testRobdd "Pengelly 18"
       (and (and (and (or (or (var 1) (var 2)) (not (var 3)))
                      (or (or (var 1) (var 2)) (var 3)))
                 (or (var 1) (not (var 2))))
            (not (var 1)))
       false
-  , testROBDD "Pengelly 19" (impl (var 1) (impl (var 2) (var 1))) true
+  , testRobdd "Pengelly 19" (impl (var 1) (impl (var 2) (var 1))) true
   , testCase "anySat of a tautology"    $ anySat (const True)  @?= Just Map.empty
   , testCase "allSat of a tautology"    $ allSat (const True)  @?= [Map.empty]
   , testCase "anySat of a contradicion" $ anySat (const False) @?= Nothing
   , testCase "allSat of a contradicion" $ allSat (const False) @?= []
   ]
 
-testROBDD s x y = testCase s $ runIdPoolM $ do
+testRobdd s x y = testCase s $ runRobddM $ do
   rx <- reduce x
   ry <- reduce y
   return $ rx @?= ry
